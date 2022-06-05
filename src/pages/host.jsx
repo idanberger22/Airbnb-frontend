18040070fe802c677502a8abdff34699fe2e4894
Yuval Kellner <yukellner@gmail.com>
2022-06-05 21:25:09 +0300

    host desig improved

--- src/pages/host.jsx
+++ src/pages/host.jsx
@@ -18,6 +18,8 @@ export const Host = () => {
     const [hostStays, setHostStays] = useState(false)
     const dispatch = useDispatch()
 
+
+
     useEffect(() => {
         document.documentElement.style.setProperty('--headerFontColor', '#000');
         document.documentElement.style.setProperty('--headerbackgroundColor', '#F7F7F7');
@@ -62,19 +64,21 @@ export const Host = () => {
                     </h1>
                 </li>
             </div>
-            <div className="header">
-                <button onClick={showUploadStayTogle}>add a listing</button>
+            {uploadStyling && <UploadStay getStays={getStays} showUploadStayTogle={showUploadStayTogle} />}
+            {!uploadStyling && <div className="header">
+                <button className="reserve-button" onClick={showUploadStayTogle}>Add a listing</button>
 
 
-            </div>
-            <section >
+            </div>}
+            {!uploadStyling &&<section >
                 <div className="card-container" >
                     {hostStays.map(stay =>
                         <StayPreview stay={stay} key={stay._id} />
-                    )}
+                        )}
                 </div>
-            </section>
-            <div>
+            </section>}
+
+            {!uploadStyling &&<div>
                 <div className="reservations-container">
                     <table className="reservations-table" >
 
@@ -96,9 +100,8 @@ export const Host = () => {
                             </tr>
                         )}
                     </table>
-                    {uploadStyling && <UploadStay getStays={getStays} showUploadStayTogle={showUploadStayTogle} />}
                 </div>
-            </div>
+            </div>}
         </div>
     </div>
     )
