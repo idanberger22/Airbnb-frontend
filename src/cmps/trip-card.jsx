a59599d8aebdb1cb0dfec6173c6a0ccf55892f00
idan berger <idanberger22@gmail.com>
2022-06-07 21:13:31 +0300

    trips

--- src/cmps/trip-card.jsx
+++ src/cmps/trip-card.jsx
@@ -1,45 +1,38 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { utilService } from "../services/util.service";
 import { ConfirmedResModal } from "./confirmed-res-modal";
 import { ConfirmedResModalTrips } from "./confirmed-res-modal-trips";
 
 export const TripCard = ({ trip }) => {
 
-    const [resModalIsOpen, setResModalIsOpen] = useState (false)
+    const [resModalIsOpen, setResModalIsOpen] = useState(false)
 
 
 
     const toggleModalIsOpen = () => {
-        
+
         setResModalIsOpen(!resModalIsOpen)
     }
-    
 
-    // onpenResModal={ModalIsOpen()}
+    console.log(trip)
 
-    // return <Link to={`/stay/${trip.stay._id}`}>
-    return <section  className="trip-card-container">
-        {resModalIsOpen && <ConfirmedResModalTrips  toggleModalIsOpen={toggleModalIsOpen} reservation={trip}/>}
+    return <section className="trip-card-container">
+        {resModalIsOpen && <ConfirmedResModalTrips toggleModalIsOpen={toggleModalIsOpen} reservation={trip} />}
         <section className="trip-card">
             <div onClick={toggleModalIsOpen} className="clickable">
                 <img src={trip.stay.img}></img>
             </div>
-            <div className="trip-card-text">
-                <div>
 
-                    <div className="trip-card-name">
-                        {trip.stay.address.country}
-                    </div>
-                    <div>
-                        hosted by {trip.hostName}
-                    </div>
-                    <div>
-                        {trip.checkIn.substring(0, 10)} - {trip.checkOut.substring(0, 10)}
-                    </div>
-                </div>
+            <div className="trip-text">
+                <p style={{textDecoration:'underline'}}>{trip.stay.name} by {trip.hostName}</p>
+                <p>Address : {trip.stay.address.street},{trip.stay.address.country}</p>
+                <p>Check in: {trip.checkIn.substring(0, 10)}</p>
+                <p>Check out : {trip.checkOut.substring(0, 10)}</p>
+                <p>Adults : {trip.adults}</p>
+                <p>Children: {trip.childrens}</p>
+                <p>Total price : ${utilService.getUsPrice(trip.totalPrice)}</p>
             </div>
         </section>
     </section>
-    // </Link >
-
 }
--- src/pages/trips.jsx
+++ src/pages/trips.jsx
@@ -1,12 +1,9 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { TripCard } from "../cmps/trip-card"
-import { TripsHeader } from "../cmps/trips-header"
 import { reservationService } from "../services/reservation.service"
-import { tripsService } from "../services/trips.service"
 import { userService } from "../services/user.service"
-
-
-
+import { utilService } from "../services/util.service"
+import { NavLink } from "react-router-dom"
 
 export const Trips = () => {
     let loggedinUser = userService.getLoggedinUser()
@@ -16,38 +13,41 @@ export const Trips = () => {
         document.documentElement.style.setProperty('--headerFontColor', '#000');
         document.documentElement.style.setProperty('--headerbackgroundColor', '#F7F7F7');
         getTrips(loggedinUser)
-    },[])
-    
+    }, [])
+
     const getTrips = async (loggedinUser) => {
-        const trips = await reservationService.query({userId: loggedinUser._id})
-        const sortedTrips = trips.sort((a,b) => Date.parse(a.checkIn) - Date.parse(b.checkIn))
+        const trips = await reservationService.query({ userId: loggedinUser._id })
+        const sortedTrips = trips.sort((a, b) => Date.parse(a.checkIn) - Date.parse(b.checkIn))
         setTrips(sortedTrips)
     }
-    if(!trips) return <h1>Loding...</h1>
+    if (!trips) return <h1>Loding...</h1>
     return (
         <section className="trips stock-margin">
             <div className="stock-margin-center">
-            <div className="flex">
-                <li>
-                    <img className="user-name-img" src={loggedinUser.imgUrl} alt="" />
-                </li>
-                <li>
-                    <h1>
-                        Hello {loggedinUser.fullName}
-                    </h1>
-                </li>
+                <div className="flex">
+                    <li>
+                        <img className="user-name-img" src={loggedinUser.imgUrl} alt="" />
+                    </li>
+                    <li>
+                        <h1>
+                            Hello {utilService.capitalizeFirst(loggedinUser.fullName)}
+                        </h1>
+                    </li>
+                </div>
+                <section className="trips-search-card noselect">
+                    <h2>Looking for the next place to visit?</h2>
+                    <h4>time to dust off your bags and start planning your next adventure</h4>
+                    <NavLink to='/explore'><button style={{ width: '200px' }} className="reserve-button">explore for more</button></NavLink>
+                </section>
+                <h2 style={{ marginBottom: '2rem' }}>Your trips</h2>
+
+                <section className="trip-card-container">
+                    {trips.map(trip => <TripCard trip={trip} key={trip._id} />)}
+                </section>
             </div>
-            {/* <div>
-                <TripsHeader />
-            </div> */}
-                <div className="trip-card-header "><h3>Your trips</h3></div>
-            <section className="trip-cards-container ">
-                {trips.map(trip => <TripCard trip={trip} key={trip._id} />)}
-            </section>
-            </div>
-                
+
         </section>
-    );
+    )
 
 }
 
